8ade49099dc574070e97cb77f1f14cf505da041c
Soham Ray <sohamray@Sohams-Laptop.local>
2025-06-25 07:49:25 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// Plain JS 
+
+const rootJS = document.getElementById("root-js")
+const p = document.createElement("p")
+p.innerText = "Hello World from JS!"
+
+rootJS.appendChild(p)
---
+++ index2.js
@@ -0,0 +1,22 @@
+/* 
+Flow for displaying HTML element through React JS:
+
+1. React JSX 
+2. The JSX gets converted into React Element (by React.createElement) 
+3. This returns a React object which contains all necessary details of the element (can be viewed in console)
+4. When we pass the object to the root, it eventually gets rendered as HTML element
+*/
+
+// Creating React elements
+// Children
+const child1 = React.createElement("span", {}, "Hello World ")
+const child2 = React.createElement("span", {}, "from React")
+
+// Parent
+const div = React.createElement("p", {}, [child1, child2])
+
+// Create Root
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// Append to the React element to the root
+root.render(div)
